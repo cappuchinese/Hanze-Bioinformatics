4f6fe0d1a35413d501bb0226ddcefdd0320ac291
cappuchinese <ljbhu2002@gmail.com>
2022-11-06 16:20:09 +0100

    BIN3: SNP assignment update

--- Thema09 - Introduction to Machine Learning/Bio-Informatica3/SNP_assignment/SNP.py
+++ Thema09 - Introduction to Machine Learning/Bio-Informatica3/SNP_assignment/SNP.py
@@ -8,29 +8,20 @@ __author__ = "Lisa Hu"
 __date__ = 09.2022
 
 # IMPORT
+from Bio import AlignIO
 import blosum
 from _data import codons
 
 
-class SeveritySNP:
+class ConservedDNA:
     """
-    TODO
+    Check how conserved the sequence is for each position (
     """
-
-    def __init__(self, proref: dict, matrix: int = 62):
-        """
-        Initialize class variables
-        :param proref: Reference protein that is aligned other sequences
-        :param matrix: Either n ϵ {45,50,62,80,90} or path to create BLOSUM matrix (default 62)
-        """
-        self.proref = proref
-        self.matrix = blosum.BLOSUM(matrix, 0)
+    def __init__(self, matrix=62):
         self.data = {}
+        self.matrix = blosum.BLOSUM(matrix, 0)
 
-    def __str__(self) -> str:
-        """
-        String representation of the class
-        """
+    def __str__(self):
         return "TODO"
 
     def _load_msa(self, filename: str):
@@ -54,6 +45,43 @@ class SeveritySNP:
         # Join the list of sequence lines to create 1 string
         for i in self.data:
             self.data[i] = "".join(self.data[i])
+
+    def _calculate_score(self):
+        # alignment = AlignIO.read(msa, "fasta")
+        # for record in alignment:
+        #     print(record)
+        scores = {}
+        score = 0
+        # Get a list of all the sequences
+        seqs = list(self.data.values())
+        # Zip through the sequences
+        x = list(zip(*seqs))
+        for i, align in enumerate(x):
+            for letter in align[1:]:
+                score += self.matrix[f"{align[0]}{letter}"]
+            scores[i] = score
+        print(scores)
+        # TODO mean center the scores
+
+
+class SeveritySNP:
+    """
+    TODO
+    """
+    def __init__(self, proref: dict, matrix: int = 62):
+        """
+        Initialize class variables
+        :param proref: Reference protein that is aligned other sequences
+        :param matrix: Either n ϵ {45,50,62,80,90} or path to create BLOSUM matrix (default 62)
+        """
+        self.proref = proref
+        self.matrix = blosum.BLOSUM(matrix, 0)
+
+    def __str__(self) -> str:
+        """
+        String representation of the class
+        """
+        return "TODO"
 
     def calc_score(self):
         """
@@ -107,3 +135,9 @@ class DNAConverter:
                 codon = seq[startpos:startpos+3]
                 protein += codons[codon]
             self.inseq[i] = protein
+
+
+if __name__ == '__main__':
+    test = ConservedDNA()
+    test._load_msa("msa.txt")
+    test._calculate_score()
